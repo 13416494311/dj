647ab43c0797f45bd00ab44c909b3cc78cf930b8
13416494311 <85470116@qq.com>
2020-09-30 06:52:27 +0800

    20200929  部分优化

--- ruoyi-ui/src/views/party/member/partyMember.vue
+++ ruoyi-ui/src/views/party/member/partyMember.vue
@@ -703,7 +703,7 @@
       };
       let checkMobile = (rule, value, callback) => {
         if (!value) {
-          return new Error("请输入电话号码");
+          callback();
         } else {
           const telReg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
           const isTelPhone = telReg.test(value);
