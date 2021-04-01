8ba4af667bb29856ec207c7f0933a0016e54876b
13416494311 <85470116@qq.com>
2021-04-01 21:53:20 +0800

    config

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://192.168.0.7:8080`,
+        target: `http://127.0.0.1:8080`,
         //target: `http://47.113.96.244`,
         changeOrigin: true,
         pathRewrite: {
